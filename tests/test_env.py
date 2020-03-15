04de18e56e8d212e271756cb6a7daf909b561e63
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-03-15 21:03:59 +0100

    ✅ add env class tests

---
+++ tests/test_env.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+
+from lib.env import World
+from lib.node import Node, State
+
+def test_uniqueness():
+    env = World(10, 10, 0.2)
+    free_tiles = env.list_available_tiles()
+    nodes = [Node(State.HEALTHY, i) for i in free_tiles]
+
+    # list of positions
+    pos = []
+    for elem in nodes:
+        pos.append(elem.tile_pos)
+
+    # check uniqueness
+    seen = set()
+    uniqueness = not any(k in seen or seen.add(k) for k in pos)
+
+    assert(uniqueness == True)
+
+def test_outofbounds():
+    env = World(10, 10, 0.2)
+    free_tiles = env.list_available_tiles()
+    new_node = [Node(State.HEALTHY, i) for i in free_tiles]
+
+    for elem in new_node:
+        assert(elem.tile_pos >= 0 and elem.tile_pos <= (env.L*env.H))
+
+def test_availability():
+    env = World(10, 10, 0.2)
+    free_tiles = env.list_available_tiles()
+
+    new_node = [Node(State.HEALTHY, i) for i in free_tiles]
+
+    for elem in new_node:
+        assert(env.w[elem.tile_pos] != State.WALL)
