73817381173acb56c05c2bf6fda4cc14a29c3d68
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-03-15 19:42:46 +0100

    🎨 prepare future structures

---
+++ lib/node.py
@@ -0,0 +1,11 @@
+'''
+author : guilyx <erwin.lejeune15@gmail.com>
+date : 15/03/2020
+licence : MIT
+brief : a node represent a person
+'''
+
+class Node():
+    def __init__(self):
+        pass
+        
