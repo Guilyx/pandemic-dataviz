8353866dd5f707db6d0c299eaa9833db7dfb4d78
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-03-15 20:41:21 +0100

    ⚡ add node state enum and sensitive conditions

--- lib/node.py
+++ lib/node.py
@@ -5,7 +5,28 @@ licence : MIT
 brief : a node represent a person
 '''
 
+from lib.env import World, ColorsBook
+
+
+class State():
+    FREE = 0
+    WALL = 1
+    HEALTHY = 2
+    INFECTED = 3
+    CURED = 4
+
+    ALLOWED_= [FREE, WALL, HEALTHY, INFECTED, CURED]
+
+
 class Node():
-    def __init__(self):
-        pass
+    def __init__(self, state, position):
+
+        self.state = state
+        self.position = position
+
+        if state in State.ALLOWED_:
+            pass
+        else:
+            print(ColorsBook.WARNING +
+                  "Careful, Node class built with wrong type for STATE variable" + ColorsBook.ENDC)
         
