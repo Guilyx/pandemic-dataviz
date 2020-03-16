fafbbb1655ee1a5c2caa9e8dbee9ae1aaf9fdee3
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-03-16 14:13:42 +0100

    🚀 add new scenarii

--- src/main.py
+++ src/main.py
@@ -16,21 +16,10 @@ def pandemic():
     env = World(20, 20)
     max_pop = len(env.list_available_tiles())
     pop = int(max_pop*0.6)
-    plague = Pandemic(1000, pop, 0.1, 0.1, 0.1, env)
+    plague = Pandemic(100000, pop, 0.1, 0.1, 0.1, env)
     plague.world.display()
-
-    rdn_pos = random.choice(list(plague.nodes.keys()))
-    rdn_node = plague.nodes[rdn_pos]
-    neighbours = rdn_node.neighbours(0, plague.world)
-    for elem in neighbours:
-        if elem not in list(plague.nodes.keys()):
-            pass
-        else:
-            print(elem)
-            plague.nodes[elem].state = State.INFECTED
-    
-    plague.evolve()
-    plague.world.display()
+    plague.spread()
+    plague.display()
 
 
 if __name__ == "__main__":
