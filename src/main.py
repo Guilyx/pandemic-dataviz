c7d8e18d9d138715d32271768c1987a6b0ed6ea9
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-03-16 18:09:45 +0100

    🔥 remove useless code

--- src/main.py
+++ src/main.py
@@ -1,8 +1,3 @@
-import numpy as np
-import matplotlib as mpl
-mpl.use('TkAgg')  # Change backend ( segfault issue )
-import matplotlib.pyplot as plt
-import seaborn as sns
 import sys
 import random
 from os import path
@@ -16,44 +11,20 @@ def main():
     env = World(10, 10, 0.2)
     env.display()
 
-
-def stack_chart():
-    env = World(20, 20)
-    max_pop = len(env.list_available_tiles())
-    pop = int(max_pop*0.6)
-
-    deathProbability = 0.04
-    recoverProbability = 0.2
-    infectedProbability = 0.7
-
-    epochs = 10000
-
-    plague = Pandemic(epochs, pop, infectedProbability,
-                      recoverProbability, deathProbability, env, 1)
-    plague.spread(False)
-
-    x = range(1, plague.hardstop+2)
-    y = [plague.healthy, plague.cured, plague.infected, plague.dead]
-
-    plt.stackplot(x, y, labels=['Unaffected', 'Cured', 'Infected', 'Dead'])
-    plt.legend(loc='upper left')
-    plt.show()
-
-
 def pandemic():
-    env = World(20, 20)
+    env = World(20, 20, 0.1)
     max_pop = len(env.list_available_tiles())
-    pop = int(max_pop*0.6)
+    pop = int(max_pop*0.7)
 
     deathProbability = 0.04
-    recoverProbability = 0.2
+    recoverProbability = 0.3
     infectedProbability = 0.7
 
-    plague = Pandemic(100000, pop, infectedProbability,
+    plague = Pandemic(1000, pop, infectedProbability,
                       recoverProbability, deathProbability, env, 1)
     plague.world.display()
     plague.spread()
 
 
 if __name__ == "__main__":
-    stack_chart()
+    pandemic()
