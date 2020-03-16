f3744c9b43bf1c11986036ec4ca9e90089dde38c
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-03-16 12:28:38 +0100

    🎨 improve display

--- lib/env.py
+++ lib/env.py
@@ -19,29 +19,29 @@ class World():
         self.H = H
 
         # the world is represented by an array with one dimension
-        self.w = [State.FREE for i in range(L*H)] # initialise every tile to empty (0)
+        self.pos_matrix = [State.FREE for i in range(L*H)] # initialise every tile to empty (0)
 
         # add walls in the first and last columns
         for i in range(H):
-            self.w[i*L] = State.WALL
-            self.w[i*L+L-1] = State.WALL
+            self.pos_matrix[i*L] = State.WALL
+            self.pos_matrix[i*L+L-1] = State.WALL
         
         # add walls in the first and last lines
         for j in range(L):
-            self.w[j] = State.WALL
-            self.w[(H-1)*L + j] = State.WALL
+            self.pos_matrix[j] = State.WALL
+            self.pos_matrix[(H-1)*L + j] = State.WALL
 
         for i in range(H):
             for j in range(L):
                 # add a wall in this tile with probability P and provided that it is neither
                 # the starting tile nor the goal tile 
                 if random() < P and not (i == 1 and j == 1) and not (i == H-2 and j == L-2):
-                    self.w[i*L+j] = State.WALL
+                    self.pos_matrix[i*L+j] = State.WALL
     
     def list_available_tiles(self):
         available_tiles = []
         for i in range(self.L*self.H):
-            if self.w[i] == State.FREE:
+            if self.pos_matrix[i] == State.FREE:
                 available_tiles.append(i)
         return(available_tiles)
 
@@ -57,7 +57,7 @@ class World():
         stdout.write("\033[0;0m")
         for i in range(self.H):
             for j in range(self.L):
-                tile = self.w[i * self.L + j]
+                tile = self.pos_matrix[i * self.L + j]
                 if (j == 0):
                     if carriage > 0:
                         stdout.write(spaces)
@@ -68,7 +68,7 @@ class World():
                     stdout.write('█')
                     stdout.write(ColorsBook.ENDC)
                 elif tile == State.HEALTHY:
-                    stdout.write(ColorsBook.OKGREEN)
+                    stdout.write(ColorsBook.OKGREEN + ColorsBook.BOLD)
                     stdout.write('o')
                     stdout.write(ColorsBook.ENDC)
                 elif tile == State.INFECTED:
