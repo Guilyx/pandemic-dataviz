8fa848c1eeeec50084190539344b71d3f2d53843
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-03-16 15:15:34 +0100

    🚧 wip : graphs

--- src/main.py
+++ src/main.py
@@ -4,22 +4,28 @@ import random
 from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-from lib.env import World
-from lib.pandemic import Pandemic
 from lib.state import State
+from lib.pandemic import Pandemic
+from lib.env import World
 
 def main():
     env = World(10, 10, 0.2)
     env.display()
 
+
 def pandemic():
     env = World(20, 20)
     max_pop = len(env.list_available_tiles())
     pop = int(max_pop*0.6)
-    plague = Pandemic(100000, pop, 0.1, 0.1, 0.1, env)
+
+    deathProbability = 0.04
+    recoverProbability = 0.1
+    infectedProbability = 0.7
+
+    plague = Pandemic(100000, pop, infectedProbability,
+                      recoverProbability, deathProbability, env, 1)
     plague.world.display()
     plague.spread()
-    plague.display()
 
 
 if __name__ == "__main__":
