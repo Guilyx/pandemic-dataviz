e50918ba2a871a938a6543c2954ae8e3e7eb16da
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-03-23 17:28:39 +0100

    ✨ add free neighbours getter

--- lib/node.py
+++ lib/node.py
@@ -57,4 +57,33 @@ class Node():
         else:
             print(ColorsBook.WARNING + "Range > 1 not yet implemented" + ColorsBook.ENDC)
 
+    def free_neighbours(self, range_, World):
+        i = self.position
+
+        if i < 0 or i >= World.L * World.H or World.pos_matrix[i] == 1:
+            # i is an incorrect tile number (outside the array or on a wall)
+            return [] 
+
+        if (range_ == 1) :
+            successors = list(filter(lambda x: World.pos_matrix[x] == State.FREE, [i - 1, 
+                                                                      i + 1, 
+                                                                      i - World.L, 
+                                                                      i + World.L, 
+                                                                      i - World.L - 1, 
+                                                                      i - World.L + 1, 
+                                                                      i + World.L - 1, 
+                                                                      i + World.L + 1]))
+            return successors
+
+        elif (range_ == 0):
+            # look in the four adjacent tiles and keep only those with no wall
+            successors = list(filter(lambda x: World.pos_matrix[x] == State.FREE, [i - 1, 
+                                                                      i + 1, 
+                                                                      i - World.L, 
+                                                                      i + World.L]))
+            return successors
+        
+        else:
+            print(ColorsBook.WARNING + "Range > 1 not yet implemented" + ColorsBook.ENDC)
+
         
