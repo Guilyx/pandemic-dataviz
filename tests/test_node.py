fe9961679f0a6215958e5916ce2ac9597a9b62b4
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-03-24 01:04:20 +0100

    👷‍ build robust tests for robust code

--- tests/test_node.py
+++ tests/test_node.py
@@ -1,1 +1,36 @@
-# no tests yet
+# -*- coding: utf-8 -*-
+
+import sys
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+
+from lib.env import World
+from lib.node import Node, State
+import random
+
+def test_position_validity():
+    for i in range(1000):
+        rdn = random.random()
+        if rdn > 0.5:
+            pos = -1*(random.randint(1, 1000))
+            node = Node(State.DEAD, pos)
+            assert(node.valid_node == False)
+        else:
+            pos = random.randint(1, 1000)
+            node = Node(State.DEAD, pos)
+            assert(node.valid_node == True)
+
+def test_state_validity():
+    for i in range(1000):
+        rdn = random.random()
+        if rdn > 0.5:
+            state = random.choice(State.ALLOWED_)
+            node = Node(state, 0)
+            assert(node.valid_node == True)
+        else:
+            state = random.randint(-1000, 1000)
+            if state in State.ALLOWED_:
+                pass
+            else:
+                node = Node(state, 0)
+                assert(node.valid_node == False)
--- tests/test_pandemic.py
+++ tests/test_pandemic.py
@@ -1,1 +1,47 @@
-# no tests yet
+# -*- coding: utf-8 -*-
+
+import sys
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+
+from lib.env import World
+from lib.pandemic import Pandemic
+import random
+
+env = World(50, 10, 0.3)
+max_pop = len(env.list_available_tiles())
+pop = int(max_pop*0.7)
+deathProbability = 0.5
+recoverProbability = 0.3
+infectedProbability = 0.8
+
+plague = Pandemic(1000, pop, infectedProbability,
+                  recoverProbability, deathProbability, env, 1)
+plague.spread()
+
+def test_population_consistancy():
+    if plague.hardstop != 0:
+        loop = plague.hardstop
+    else:
+        loop = plague.epochs
+    
+    for i in range(loop):
+        assumed_pop = plague.dead[i] + plague.cured[i] + plague.healthy[i] + plague.infected[i] 
+        assert(assumed_pop == plague.population)
+
+def test_negative_population():
+    corona = Pandemic(1000, -3, 0.1, 0.1, 0.1, env)
+    assert(corona.population > 0)
+
+def test_population_overflow():
+    corona = Pandemic(1000, 9999999999999999999999, 0.1, 0.1, 0.1, env)
+    assert(corona.population == len(env.list_available_tiles()))
+
+def test_negative_initial_cases():
+    corona = Pandemic(1000, -3)
+
+
+# test negative probabilities
+# test negative number of initial cases
+# test negative spreadRange
+# test unsupported spreadRange
